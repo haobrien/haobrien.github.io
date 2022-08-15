cc71b1c2ae158b22b2d3039749e4827c13276723
Heather OBrien <haobrientx@gmail.com>
2022-08-15 15:16:05 -0500

    Revert image change

--- src/config/projects.js
+++ src/config/projects.js
@@ -2,7 +2,7 @@ const projects = [
     {
         name: 'Color Palette App',
         description: 'Browse predefined color palettes, or create your own! Click to copy your color code for use in your own projects. Created with React.js.',
-        img: '/color-app.PNG',
+        img: '/color-app.png',
         demoUrl: 'https://haobrien.github.io/color-palette-app/',
         repoUrl: 'https://github.com/haobrien/color-palette-app',
         skills: ['React', 'JavaScript', 'HTML', 'CSS', 'Material UI', 'Responsive']
@@ -10,7 +10,7 @@ const projects = [
     {
         name: 'YelpCamp',
         description: 'Yelp-style website for posting and rating campsites. Register your own account, or use username & password demo for login.',
-        img: '/yelpcamp.PNG',
+        img: '/yelpcamp.png',
         demoUrl: 'https://cryptic-sea-93579.herokuapp.com/',
         repoUrl: 'https://github.com/haobrien/yelp-camp-v2',
         skills: ['JavaScript', 'ExpressJS', 'MongoDB', 'Bootstrap', 'NodeJS', 'EJS', 'Responsive']
@@ -26,7 +26,7 @@ const projects = [
     {
         name: 'Lights Out',
         description: 'A virtual recreation of the 90s classic, Lights Out! Turn out all the lights to win. Refresh the page to start a new game.',
-        img: 'lights-out.PNG',
+        img: 'lights-out.png',
         demoUrl: 'https://haobrien.github.io/lights-out/',
         repoUrl: 'https://github.com/haobrien/lights-out',
         skills: ['React', 'JavaScript', 'HTML', 'CSS']
@@ -34,7 +34,7 @@ const projects = [
     {
         name: 'Deck of Cards',
         description: 'A virtual whole deck of cards. Draw one card at a time. After all 52 cards have been drawn, you can receive a new shuffled deck.',
-        img: 'deckofcards.PNG',
+        img: 'deckofcards.png',
         demoUrl: 'https://haobrien.github.io/deck-of-cards/',
         repoUrl: 'https://github.com/haobrien/deck-of-cards',
         skills: ['React', 'JavaScript', 'HTML', 'CSS', 'Mobile Friendly']
@@ -42,7 +42,7 @@ const projects = [
     {
         name: 'Portfolio',
         description: "You're looking at it! Built using React and Material UI. Currently working on making it more usable for other developers.",
-        img: '/portfolio.PNG',
+        img: '/portfolio.png',
         demoUrl: '',
         repoUrl: 'https://github.com/haobrien/haobrien.github.io',
         skills: ['React', 'JavaScript', 'HTML', 'CSS', 'Material UI', 'Responsive']
